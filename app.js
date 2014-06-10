88d72e99f3ec24bffb815471fab6dc5a124d1e46
clintwood (Office) <clint@anotherway.co.za>
2014-06-10 08:28:08 +0200

    Express version...

--- app.js
+++ app.js
@@ -4,32 +4,25 @@
 
 // core modules
 var path = require('path');
-var koa = require('koa');
+var express = require('express');
 
 // local deps
 var bundler = require('./lib/app-bundler');
 
 // create koa app (exported)
-var app = module.exports = koa();
-app.name = 'Koa based SPA app.';
+var app = module.exports = express();
 
 var publicDir = path.join(__dirname, 'public');
 var privateDir = path.join(__dirname, 'private');
 
 // middlewares
 
-// global error handling
-app.use(function* errors(next) {
-  try {
-    yield* next;
-  } catch (err) {
-    this.status = err.status || 500;
-    this.body = err.message;
-    this.app.emit('error', err, this);
-  }
-});
-
 app.use(bundler({
   root: path.join(privateDir, 'app'),
   build: publicDir
 }))
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
--- index.js
+++ index.js
@@ -5,11 +5,8 @@
 
 var app = module.exports = require('./app');
 
-// spin up app
-if (!module.parent) {
-  var port = process.env.PORT || 3000;
-  app.listen(port, function(err) {
-    if (err) throw err;
-    console.log('%s listening on port %s.', (app.name || 'SPA Site'), port);
-  });
-}
+var port = process.env.PORT || 3000;
+app.listen(port, function(err) {
+  if (err) throw err;
+  console.log('%s listening on port %s.', (app.name || 'SPA Site'), port);
+});
